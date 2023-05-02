1315a77036be6e0af75e5b9fa3d83dabc87a286c
Ayesha <ayesha18mbstu@gmail.com>
2023-05-02 20:35:13 +0600

    display data on new server

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const chefs = require('./data/chefs.json')
 
 app.get('/', (req, res) => {
   res.send('The Kitchen Diaries server!')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
 })
 
 app.listen(port, () => {

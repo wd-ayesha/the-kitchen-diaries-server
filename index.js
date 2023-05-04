0354e48c18e81a85f51a901e4a4f98fd955d17f5
Ayesha <ayesha18mbstu@gmail.com>
2023-05-04 23:23:37 +0600

    data load by id

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
 
+app.get('/chefs/:id', (req, res) => {
+  const id = req.params.id;
+  const item = chefs?.bangladeshi_chefs?.find(detail => detail.id == id);
+  res.send({item});
+})
+
 app.listen(port, () => {
   console.log(`server is running on port ${port}`)
 })
